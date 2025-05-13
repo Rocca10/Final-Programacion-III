19c50656f2ced47d0f6b51a1c7804024d3d82b18
Nicolas <nicolas.rocca10@gmail.com>
2025-05-13 19:07:22 -0300

    Se necesita Login para ver las paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,93 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Recetas from './pages/Recetas';
-import Ingredientes from './pages/Ingredientes';
-import BuscarRecetas from './pages/BuscarRecetas';
-import CrearReceta from './components/CrearReceta';
-import Registro from './pages/Registro';
-import UsuariosAdmin from './pages/UsuariosAdmin';
-import Perfil from './pages/Perfil';
-import RecetaDetalle from './pages/RecetaDetalle';
-import ScrollToTop from './components/ScrollToTop';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Recetas from "./pages/Recetas";
+import Ingredientes from "./pages/Ingredientes";
+import BuscarRecetas from "./pages/BuscarRecetas";
+import CrearReceta from "./components/CrearReceta";
+import Registro from "./pages/Registro";
+import UsuariosAdmin from "./pages/UsuariosAdmin";
+import Perfil from "./pages/Perfil";
+import RecetaDetalle from "./pages/RecetaDetalle";
+import ScrollToTop from "./components/ScrollToTop";
+import RutaPrivada from "./components/RutaPrivada";
 
 function App() {
   return (
     <BrowserRouter>
-    <ScrollToTop />
+      <ScrollToTop />
       <Routes>
+        {/* Públicas */}
         <Route path="/" element={<Login />} />
-        <Route path="/Registro" element={<Registro />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/recetas" element={<Recetas />} />
-        <Route path="/ingredientes" element={<Ingredientes />} />
-        <Route path='/buscar-recetas' element={<BuscarRecetas/>} />
-        <Route path='/crear-receta' element={<CrearReceta/>}/>
-        <Route path='/admin/usuarios' element={<UsuariosAdmin/>}/>
-        <Route path='/perfil' element={<Perfil/>}/>
-        <Route path="/recetas/:id" element={<RecetaDetalle />} />
+        <Route path="/registro" element={<Registro />} />
 
+        {/* Privadas */}
+        <Route
+          path="/home"
+          element={
+            <RutaPrivada>
+              <Home />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/recetas"
+          element={
+            <RutaPrivada>
+              <Recetas />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/ingredientes"
+          element={
+            <RutaPrivada>
+              <Ingredientes />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/buscar-recetas"
+          element={
+            <RutaPrivada>
+              <BuscarRecetas />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/crear-receta"
+          element={
+            <RutaPrivada>
+              <CrearReceta />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/admin/usuarios"
+          element={
+            <RutaPrivada>
+              <UsuariosAdmin />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/perfil"
+          element={
+            <RutaPrivada>
+              <Perfil />
+            </RutaPrivada>
+          }
+        />
+        <Route
+          path="/recetas/:id"
+          element={
+            <RutaPrivada>
+              <RecetaDetalle />
+            </RutaPrivada>
+          }
+        />
+         {/* Ruta no encontrada redirige al login */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
